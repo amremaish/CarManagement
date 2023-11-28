be637af0fddced61d39e029b6eac930dc10001c1
Amr Emaish <amr.emaish@gmail.com>
2023-11-29 01:09:48 +0200

    fix login and provide the missing methods

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class DatabaseManager:
     def __init__(self):
         self.mydb = mysql.connector.connect(
@@ -20,7 +21,8 @@ class DatabaseManager:
                                 id INT AUTO_INCREMENT PRIMARY KEY,
                                 name VARCHAR(255),
                                 email VARCHAR(255),
-                                phone VARCHAR(20))''')
+                                phone VARCHAR(20),
+                                password VARCHAR(255))''')
 
         self.mycursor.execute('''CREATE TABLE IF NOT EXISTS vehicles (
                                 id INT AUTO_INCREMENT PRIMARY KEY,
@@ -39,6 +41,19 @@ class DatabaseManager:
 
         self.mydb.commit()
 
+    def check_email_exists(self, email):
+        self.mycursor.execute("SELECT * FROM customers WHERE email = %s", (email,))
+        result = self.mycursor.fetchone()
+        if result:
+            return True
+        else:
+            return False
+
+    def get_customer_by_email(self, email):
+        query = "SELECT * FROM customers WHERE email = %s"
+        self.mycursor.execute(query, (email,))
+        return self.mycursor.fetchone()
+
     def check_availability(self, vehicle_id, date_hired, date_returned):
         self.mycursor.execute('''SELECT * FROM bookings
                                 WHERE vehicle_id = %s AND
@@ -47,9 +62,10 @@ class DatabaseManager:
                               (vehicle_id, date_hired, date_returned, date_hired, date_returned))
         return self.mycursor.fetchone()
 
-    def insert_customer(self, name, email, phone):
-        self.mycursor.execute('''INSERT INTO customers (name, email, phone) VALUES (%s, %s, %s)''',
-                              (name, email, phone))
+    def create_customer(self, name, email, phone, password):
+        sql = "INSERT INTO customers (name, email, phone, password) VALUES (%s, %s, %s, %s)"
+        val = (name, email, phone, password)
+        self.mycursor.execute(sql, val)
         self.mydb.commit()
         return self.mycursor.lastrowid
 
@@ -62,4 +78,6 @@ class DatabaseManager:
     def close_connection(self):
         self.mydb.close()
 
+
 db_operations = DatabaseManager()
+db_operations.create_tables()
--- routes.py
+++ routes.py
@@ -26,10 +26,11 @@ def signup():
     hashed_password = generate_password_hash(password)
 
     if db_operations.check_email_exists(email):
-        return jsonify({'message': 'Email already exists. Please log in.'}), 400
+        return jsonify({'message': 'Email already exists.'}), 400
 
     db_operations.create_customer(name, email, phone, hashed_password)
     return jsonify({'message': 'Signup successful'}), 201
+
 
 @routes.route('/login', methods=['POST'])
 def login():
@@ -44,6 +45,10 @@ def login():
     if not user or not check_password_hash(user[4], password):
         return jsonify({'message': 'Invalid credentials'}), 401
 
-    token = jwt.encode({'email': email, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)},
-                       os.getenv("JWT_SECRET"))  # Change this to your secret key
-    return jsonify({'token': token.decode('UTF-8')}), 200
+    token = jwt.encode(
+        {
+            'email': email,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=int(os.getenv("JWT_EXPIRE_TIME")))
+        }, os.getenv("JWT_SECRET"))
+
+    return jsonify({'access_token': token}), 200
