bed7cecd92a76f28d24790aca302d502e037cac1
Amr Emaish <amr.emaish@gmail.com>
2023-12-04 05:39:08 +0200

    modify creating an admin user to get db operation

--- cli.py
+++ cli.py
@@ -1,51 +1,24 @@
-import os
 import sys
-import mysql.connector
 from getpass import getpass  # For secure password input
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash
+from database import db_operations
 
 load_dotenv()
 
 
 def create_admin_user():
-    try:
-        # Collect user inputs for admin creation
-        name = input("Enter name: ")
-        email = input("Enter email: ")
-        phone = input("Enter phone: ")
-        password = getpass("Enter password: ")
-
-        # Establish connection to your MySQL database
-        mydb = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME")
-        )
-
-        mycursor = mydb.cursor()
-
-        insert_query = '''
-            INSERT INTO users (name, email, phone, password, is_admin)
-            VALUES (%s, %s, %s, %s, %s)
-        '''
-
-        hashed_password = generate_password_hash(password)
-
-        mycursor.execute(insert_query, (name, email, phone, hashed_password, True))
-        mydb.commit()
-
-        print("Admin user created successfully!")
-
-    except mysql.connector.Error as err:
-        print("Error creating admin user:", err)
-
-    finally:
-        # Close database connection
-        if 'mydb' in locals() and mydb.is_connected():
-            mycursor.close()
-            mydb.close()
+    # Collect user inputs for admin creation
+    name = input("Enter name: ")
+    email = input("Enter email: ")
+    phone = input("Enter phone: ")
+    password = getpass("Enter password: ")
+    hashed_password = generate_password_hash(password)
+    if db_operations.check_email_exists(email):
+        print('Email already exists.')
+        return
+    db_operations.create_user(name, email, phone, hashed_password)
+    print("Admin user created successfully!")
 
 
 if __name__ == "__main__":
